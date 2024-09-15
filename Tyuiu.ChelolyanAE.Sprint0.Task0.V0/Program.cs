68427778088e05cedc0511dbad419dbfab6de2df
79829 <79829@LAPTOP-UC0EC82M>
2024-09-15 17:45:24 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.ChelolyanAE.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.ChelolyanAE.Sprint0.Task0.V0_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
