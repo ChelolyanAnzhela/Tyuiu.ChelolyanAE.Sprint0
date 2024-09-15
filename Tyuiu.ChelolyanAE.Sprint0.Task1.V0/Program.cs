45fc232a0ff95ab4a9de72cc2deba1c96fcefc82
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-09-15 18:17:57 +0500

    Task1

---
+++ Tyuiu.ChelolyanAE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.ChelolyanAE.Sprint0.Task1.V0_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Анжела";
+            int age = 20;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
